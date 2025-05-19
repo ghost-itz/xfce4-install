55768691ad183a298cbeb7c666ae8245a8090f35
ghost <46002572+ghost-itz@users.noreply.github.com>
2025-05-19 02:07:13 -0300

    Create posinstall.sh

---
+++ posinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo pacman -S parole evince xfce4-pulseaudio-plugin xfce4-screenshooter \
+               plugin-sensores-xfce4 libxnvctrl gnome-disk-utility gparted \
+               baobab \
+               htop \
+               go \
+               wget \
+               flatpak \
+               audacious \
+               gst-libav gst-plugins-ugly gst-plugins-good gst-plugins-bad \
+               vlc \
+               qbittorrent \
+               mousepad ristretto plugin-xfce4-cpugraph plugin xfce4-weather plugin xfce4-whiskermenu
